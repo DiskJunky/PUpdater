4806f6f4b280f768c2838db35dd346cb227f41eb
DiskJunky <diskjunky@hotmail.com>
2023-11-08 19:27:29 +0000

    Created bas project structure
    Created the initial project structure

---
+++ PUpdater.CmdLine/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PUpdater.CmdLine
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
