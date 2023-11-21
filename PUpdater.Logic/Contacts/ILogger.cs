40e72e7daa643dd0702f9585ef4b2230354bc7dc
DiskJunky <diskjunky@hotmail.com>
2023-11-21 20:28:28 +0000

    Added initial logging elements

---
+++ PUpdater.Logic/Contacts/ILogger.cs
@@ -0,0 +1,24 @@
+﻿namespace PUpdater.Logic.Contacts
+{
+    /// <summary>
+    /// This provides a general template for a logger implementation.
+    /// </summary>
+    public interface ILogger
+    {
+        void Trace(string message, params object[] args);
+
+        void Debug(string message, params object[] args);
+
+        void Info(string message, params object[] args);
+
+        void Warn(string message, params object[] args);
+
+        void Error(string message, params object[] args);
+
+        void Error(Exception exception, string message, params object[] args);
+
+        void Fatal(string message, params object[] args);
+
+        void Fatal(Exception exception, string message, params object[] args);
+    }
+}
